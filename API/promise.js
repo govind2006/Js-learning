6e87c14fca5f253dcac8fa4fac87f43719ac2517
GOVIND KUMAR <39936384+govind2006@users.noreply.github.com>
2024-05-30 04:54:26 +0000

    API

--- API/promise.js
+++ API/promise.js
@@ -23,7 +23,7 @@ async function consumePromise5(){
    }
 }
 
-consumePromise5()
+// consumePromise5()
 
 async  function getallusers(){
     try{
@@ -35,9 +35,48 @@ async  function getallusers(){
     }
 }
 // getallusers()
+let cnt=0;
+let year=2011;
+// fetch(`https://jsonmock.hackerrank.com/api/football_matches?year=${year}`).then((response)=>{
+//     return response.json()
+// }).then((res)=>{
+//     const resdata = res.data;
+//     for(let i=0;i<resdata.length;i++){
+//         if(resdata[i].year==2011 && resdata[i].team1goals==resdata[i].team2goals){
+//             cnt++;
+//         }
+//     }
+//     console.log(`draw match = ${cnt}`);
+// }).catch((error)=>console.log(error))
 
-fetch('https://api.github.com/users/govind2006').then((response)=>{
-    return response.json()
-}).then((data)=>{
-    console.log(data)
-}).catch((error)=>console.log(error))
+
+
+let comp = 'UEFA Champions League';
+
+const getallwin = async function(win) {
+    console.log(comp, year, win);
+    const res = await fetch(`https://jsonmock.hackerrank.com/api/football_matches?competition=${comp}&year=${year}&team1=${win}`);
+    const data = await res.json();
+    console.log(res);
+    return "ok";
+};
+
+const titlewin = async function() {
+    const res = await fetch(`https://jsonmock.hackerrank.com/api/football_competitions?name=${comp}&year=${year}`);
+    const data = await res.json();
+    return data.data[0].winner;
+};
+
+titlewin()
+    .then(winner => {
+        return getallwin(winner);
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+
+
--- classes_and_oop/call.js
+++ classes_and_oop/call.js
@@ -1,15 +1,40 @@
-function setUserName(username){
-    this.username=username
-    return this;
+// function setUserName(username){
+//     this.username=username
+//     return this;
+// }
+
+// function createUser(username,email,password){
+//     setUserName.call(this,username)
+//     // this.username= setUserName.call(username);
+//     this.email=email
+//     this.password=password
+// }
+
+// const u1 = new createUser("govind","abc@g.com","123")
+
+// console.log(u1);
+
+const fetch = require("node-fetch");
+
+async function getDrawnMatches(year) {
+    let goals=[];
+    let ans=0;
+    for(let goal=0;goal<=10;goal++)
+    {
+        const myPromise = fetch(`https://jsonmock.hackerrank.com/api/football_matches?year=${year}&team1goals=`+goal+`&team2goals=`+goal)
+                            .then(res => res.json())
+                            .then(data => {
+                                ans+=data.total;
+                                console.log(data.total,goal);
+                                return data.total
+                            });
+
+        goals.push(myPromise);
+    }
+    await Promise.all(goals)
+    return ans;
 }
-
-function createUser(username,email,password){
-    setUserName.call(this,username)
-    // this.username= setUserName.call(username);
-    this.email=email
-    this.password=password
-}
-
-const u1 = new createUser("govind","abc@g.com","123")
-
-console.log(u1);
+getDrawnMatches
+    (2011).then((ans)=>{
+        console.log(ans);
+    })
