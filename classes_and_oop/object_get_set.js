a1bce2e992d744c977e06a0d28ec6266b8c9927f
GOVIND KUMAR <39936384+govind2006@users.noreply.github.com>
2024-10-03 08:21:05 +0000

    closure

--- classes_and_oop/object_get_set.js
+++ classes_and_oop/object_get_set.js
@@ -1,15 +1,16 @@
 const User = {
     _email:'hello@ge.com',
-    _password:'123',
+    _password:"123",
 
     get email(){
-        return this._email.toUpperCase
+        return this._email.toUpperCase()
     },
 
     set email(value){
         this._email=value
-    },
-
-    
-
+    } 
 }
+
+const u1 = Object.create(User)
+
+console.log("email: ",u1.email);
